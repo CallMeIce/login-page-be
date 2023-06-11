70a0ffcdb388f8e41789217e5f74d8f0ff19b579
Isaak Morales <ice.isaak97@gmail.com>
2023-06-11 15:54:59 -0400

    first commit

---
+++ src/main/java/com/example/loginformbe/LoginFormBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginformbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginFormBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginFormBeApplication.class, args);
+    }
+
+}
